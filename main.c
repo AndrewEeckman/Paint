ccfae0ae4826534010d339253ff6916902bf79c7
Andrew Eeckman <andreweeckman@campus-028-065.ucdavis.edu>
2017-12-05 00:18:37 -0800

    Adding the Project

---
+++ main.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+//void checkConditions(int argc, char **argv);
+
+bool inputChecker(int passArgs);
+
+int main(int argc, char** argv) {
+    const char blankSpace = '*';
+    const char pieces[] = "\/|-+";
+
+    if (inputChecker(argc)) {
+        return 0;
+    }
+
+    //checkConditions(argc, argv);
+
+    int r = atoi(argv[1]);
+    int c = atoi(argv[2]);
+    int w = atoi(argv[3]);
+
+    const int numRows = r;
+    const int numCols = c;
+
+    int rowPlayed;
+    int colPlayed;
+
+    char** board = createBoard(numRows, numCols, blankSpace);
+
+    int playerTurn = 0;
+
+    do {
+
+        displayBoard(board, numRows, numCols);
+
+        getMove(board, numRows, numCols, blankSpace, &rowPlayed, &colPlayed);
+        makeMove(board, rowPlayed, colPlayed, pieces[playerTurn]);
+        playerTurn = changeTurn(playerTurn);
+
+    } while(!isGameOver(board, numRows, numCols, blankSpace, w));
+
+    displayBoard(board, numRows, numCols);
+    declareEndResult(playerTurn, board, numRows, numCols, blankSpace, w);
+
+    cleanUp(&board, numRows, numCols);
+
+    return 0;
+}
+
+bool inputChecker(int passArgs) {
+    if (passArgs < 4) {
+        printf("Not enough arguments entered\n");
+        printf("Usage connectn.out num_rows num_columns");
+        printf(" number_of_pieces_in_a_row_needed_to_win");
+        return true;
+    }
+    if (passArgs > 4) {
+        printf("Too many arguments entered\n");
+        printf("Usage connectn.out num_rows num_columns");
+        printf(" number_of_pieces_in_a_row_needed_to_win");
+        return true;
+    }
+    return false;
+}
