883400d3f669e0083f034fa410270876f7b7b4b7
Tucker <tzischka17@tzschka17-osx64.local>
2017-12-06 15:19:25 -0800

    Finishing the last of the needed commits

--- draw.c
+++ draw.c
@@ -2,3 +2,14 @@
 // Created by Tucker on 12/6/17.
 //
 
+int pickABrush(int xCoord1, int yCoord1, int xCoord2, int yCoord2) { //Function in charge of picking draw funciton is going to be used
+    if () {
+
+    } else if (){
+
+    } else if (){
+
+    } else if () {
+
+    }
+}
