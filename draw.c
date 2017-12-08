bead7968e131b7e535be1f5967e9f85ffb7e469a
Tucker <tzischka17@campus-016-223.ucdavis.edu>
2017-12-07 23:20:15 -0800

    Finishing the end master branch commits

--- draw.c
+++ draw.c
@@ -3,13 +3,28 @@
 //
 
 int pickABrush(int xCoord1, int yCoord1, int xCoord2, int yCoord2) { //Function in charge of picking draw funciton is going to be used
-    if () {
+    if () { //Horizontal line drawing
+
+    } else if (){ //Vertical Line Drawing
 
     } else if (){
+        /*      /
+         *    /
+         *  /
+         */
 
-    } else if (){
+
 
     } else if () {
+        /*\
+         * \
+         *  \
+         *   \
+         */
+
+        for(int i = xcoord1)
+
+
 
     }
 }
