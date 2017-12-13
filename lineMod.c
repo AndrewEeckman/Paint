009dc750a2aeced3d990dcf471627cdb88ef0b07
Tucker <tzischka17@campus-045-025.ucdavis.edu>
2017-12-12 20:04:22 -0800

    Added a row Add function

--- lineMod.c
+++ lineMod.c
@@ -6,20 +6,27 @@
 #include <stdlib.h>
 
 
-void lineAdd (char ** canvas, int * num_rows, int * num_cols, int * new_rows, int * new_cols, const char blank_space, const int rowNumber) {
-        char * holder = canvas[*num_rows-1];
-        char ** newCanvas = (char**)malloc((*num_rows + 1) * sizeof(char*));
-    for( int row = 0; row <= (*num_rows + 1); row++)                                         {
-        canvas[*num_rows - row] = canvas[*num_rows - row - 1];
-
+void rowAdd (char ** canvas, int * num_rows, int * num_cols, int * new_rows, int * new_cols, const char blank_space, const int rowNumber) {
+    char ** tempCanvas = (char**)malloc((*num_rows) * sizeof(char*));
+        for(int i = 0; i <*num_rows; i++) {
+            tempCanvas[i] = (char*)malloc((*num_cols) * sizeof(char*));
+            for(int j = 0; j < *num_cols; j ++){
+                canvas[i][j] = tempCanvas[i][j];
+            }
+        }
+    canvas = (char**)realloc(canvas, (*num_rows+1)*sizeof(char*));
+    for( int row = 0; row <= (*num_rows + 1); row++) {
+        canvas[row] = (char*)realloc(canvas[row], sizeof(char*));
+            for (int col = 0; col <= *num_cols; col++) {
+                if (row == rowNumber) {
+                    canvas[row][col] = blank_space;
+                } else {
+                    canvas[row][col] = tempCanvas[row][col];
+                }
+            }
+        free(tempCanvas[row]);
     }
-
-    // last line -> holder
-    // 2nd to last line ->  last line
-    // 3rd to last -> 2nd to last
-    // line 3
-
-
+    free(tempCanvas);
 }
 
 
--- lineMod.h
+++ lineMod.h
@@ -5,6 +5,7 @@
 #ifndef PAINT_LINEMOD_H
 #define PAINT_LINEMOD_H
 
+void rowAdd (char ** canvas, int * num_rows, int * num_cols, int * new_rows, int * new_cols, const char blank_space, const int rowNumber);
 
 
 #endif //PAINT_LINEMOD_H
